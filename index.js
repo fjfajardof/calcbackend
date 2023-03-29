e66c4aa2b8ba61909ddb341fcf94b9e15c33e770
Fabian Fajardo <fabianjff97@gmail.com>
2023-03-29 14:44:52 -0500

    V 0.0.2

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-const express= require("express");
+//1. Crear un objeto express
+const express=require('express');
 
-const app=express ();
+//2. Crear un objeto que represente nuestra aplicación
+const app= express();
 
+app.use(express.json());
+
+//Definir los entry point de la API
+//Definir la ruta (la url) en dónde va a responder nuestra API
+// http://localhost:3000/
+
+
+app.get(
+    '/api/sumar',
+    //Se requieren dos objetos: uno representando la petición
+    //un objeto representando la respuesta
+    (req, res)=>{
+        //To Do: Aquí va el procesamiento de la petición a esta ruta
+        console.log("Alguien está conectándose a esta ruta!!");        
+        res.json(req.body);
+    }
+);
+
+app.post(
+    '/api/restar',
+    (req, res)=>{
+        const {numero_1, numero_2}= req.body;
+        const resultado=numero_1-numero_2;        
+        res.json(resultado);
+    }
+);
+
+//¿Cómo queda la ruta para dividir?
+
+app.post(
+    '/api/dividir',
+    (req,res)=>{
+        let resultado;
+        
+        try{
+            const {numero_1, numero_2}= req.body;           
+            resultado=numero_1/numero_2;   
+
+        }catch(error){
+            //Gestionar el error
+            resultado=error;
+        }
+        
+        res.json(resultado);
+    }
+)
+
+//3. Crear un servicio para escuchar peticiones
 app.listen(
-    3000,
+    3001,
     ()=>{
-        console.log("Puerto:3000");
+      console.log("Servidor ejecutándose en el puerto 3001");
     }
 );
